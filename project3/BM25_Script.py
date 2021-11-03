1d650cffd9f433615c03957dd8fcebdcb746e9cd
akshayamohan <imsunitha@gmail.com>
2021-11-03 18:58:19 -0400

    changed

--- project3/BM25_Script.py
+++ project3/BM25_Script.py
@@ -189,14 +189,14 @@ class Indexer:
 
     def query_solr(self):
         # change the url according to your own corename and query
-        inurl = 'http://localhost:8983/solr/IRF21_VSM/select?q=text_en%3ASyrian%20civil%20war%20OR%20text_de%3ASyrian%20civil%20war%20OR%20text_ru%3ASyrian%20civil%20war&fl=id%2Cscore&wt=json&indent=true&rows=20'
-        outfn = '2_bm.txt'
+        inurl = 'http://localhost:8983/solr/IRF21_BM25/select?q=text_en%3ARussische%20Botschaft%20in%20Syrien%20von%20Granaten%20getroffen%20OR%20text_de%3ARussische%20Botschaft%20in%20Syrien%20von%20Granaten%20getroffen%20OR%20text_ru%3ARussische%20Botschaft%20in%20Syrien%20von%20Granaten%20getroffen&fl=id%2Cscore&wt=json&indent=true&rows=20'
+        outfn = '4_bm.txt'
 
         #inurl = 'http://localhost:8983/solr/IRF21_BM25/select?q=text_en%3ADavid%20Cameron%20urged%20to%20ensure%20vulnerable%20Syrian%20refugees%20are%20settled%20by%20winter%20OR%20text_de%3ADavid%20Cameron%20urged%20to%20ensure%20vulnerable%20Syrian%20refugees%20are%20settled%20by%20winter%20OR%20text_ru%3ADavid%20Cameron%20urged%20to%20ensure%20vulnerable%20Syrian%20refugees%20are%20settled%20by%20winter&fl=id%2Cscore&wt=json&indent=true&rows=20'
 
 
         # change query id and IRModel name accordingly
-        qid = '002'
+        qid = '004'
         IRModel='bm25' #either bm25 or vsm
         outf = open(outfn, 'a+')
         # data = urllib2.urlopen(inurl)
