f876edfb909261d7db3b77d04b4497f790dbabbb
akshayamohan <imsunitha@gmail.com>
2021-11-04 20:10:27 -0400

    changed

--- project3/test.py
+++ project3/test.py
@@ -29,44 +29,40 @@ core_names = ["IRF21_BM25", "IRF21_VSM"]
 
 def query_solr():
 
-        print("inside new code")
-        with open('test-queries.txt') as fp:
+    print("inside new code")
+    with open('test-queries.txt') as fp:
+
+        for core in core_names:
             for line in fp:
                 query_id, query = line.split(' ', 1)
                 query = re.sub(r'[^\w\s]', ' ', query)
                 query = re.sub(' +', ' ', query)
                 query = query.strip()
                 encoded_query = urllib.parse.quote(query)
-                print (encoded_query)
 
-                for core in core_names:
-
-                    inurl = 'http://localhost:8983/solr/' + core + '/select?fl=id%2Cscore&q=text_en%3A(' + encoded_query + ')%20or%20text_de%3A(' + encoded_query + ')%20or%20text_ru%3A(' + encoded_query + ')' + '&rows=20&wt=json'
-    
-                    outfn = str(int(query_id)) + '_' + core + '.txt'
+                inurl = 'http://localhost:8983/solr/' + core + '/select?fl=id%2Cscore&q=text_en%3A(' + encoded_query + ')%20or%20text_de%3A(' + encoded_query + ')%20or%20text_ru%3A(' + encoded_query + ')' + '&rows=20&wt=json'
+                outfn = str(int(query_id)) + '_' + core + '.txt'
 
 
-                    # change query id and IRModel name accordingly
-                    # qid = '002'
+                # change query id and IRModel name accordingly
+                if core == "IRF21_BM25":
+                    IRModel = 'bm25'
+                else:
+                    IRModel = 'vsm'
 
-                    if core == "IRF21_BM25":
-                        IRModel = 'bm25'
-                    else:
-                        IRModel = 'vsm'
+                # IRModel='bm25' #either bm25 or vsm
+                outf = open(outfn, 'a+')
+                # data = urllib2.urlopen(inurl)
+                # if you're using python 3, you should use
+                data = urllib.request.urlopen(inurl)
 
-                    # IRModel='bm25' #either bm25 or vsm
-                    outf = open(outfn, 'a+')
-                    # data = urllib2.urlopen(inurl)
-                    # if you're using python 3, you should use
-                    data = urllib.request.urlopen(inurl)
-
-                    docs = json.load(data)['response']['docs']
-                    # the ranking should start from 1 and increase
-                    rank = 1
-                    for doc in docs:
-                        outf.write(query_id + ' ' + 'Q0' + ' ' + str(doc['id']) + ' ' + str(rank) + ' ' + str(doc['score']) + ' ' + IRModel + '\n')
-                        rank += 1
-                    outf.close()
+                docs = json.load(data)['response']['docs']
+                # the ranking should start from 1 and increase
+                rank = 1
+                for doc in docs:
+                    outf.write(query_id + ' ' + 'Q0' + ' ' + str(doc['id']) + ' ' + str(rank) + ' ' + str(doc['score']) + ' ' + IRModel + '\n')
+                    rank += 1
+                outf.close()
 
 
 query_solr()
